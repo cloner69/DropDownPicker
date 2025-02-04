2ceb7dca3a88b5b794d3cb07eee64c490fd13721
AdrianSA <50012235+cloner69@users.noreply.github.com>
2025-02-04 10:54:49 +0700

    Drop Down Picker

---
+++ DropDownPicker/DropDownPicker/DropDownPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropDownPickerApp.swift
+//  DropDownPicker
+//
+//  Created by Adrian Suryo Abiyoga on 23/01/25.
+//
+
+import SwiftUI
+
+@main
+struct DropDownPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
